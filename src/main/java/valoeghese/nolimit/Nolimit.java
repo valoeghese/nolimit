3ea28ce13cf305565b661cbe2f185c3b788c311c
valoeghese <valoeghesemods@gmail.com>
2020-12-12 10:48:55 +1300

    start nolimit

---
+++ src/main/java/valoeghese/nolimit/Nolimit.java
@@ -0,0 +1,17 @@
+package valoeghese.nolimit;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nolimit implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Nolimit");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}
